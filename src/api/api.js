adfd39553f7f4af2528d0c52b36c622ab648ca7d
kpaveliev <kpaveliev@gmail.com>
2022-07-29 13:35:11 +0300

    changed baseurl in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getCookie } from "../utils/cookies";
 
 export default axios.create({
-  baseURL: `http://127.0.0.1:5000`,
+  baseURL: `http://kpaveliev-skypro.cf:5000`,
   // withCredentials: true,
   headers: {
     Authorization: "Bearer " + getCookie("AccessToken")
